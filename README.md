de08454a0d4a7046e92d652c10d4c2c57150a148
donovan6000 <nicolasflamel@mwcwallet.com>
2024-11-30 00:42:38 -0800

    Adds support for MWC Pay's payment notes and API key

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ from nicolasflamel.mwc_pay import MwcPay
 mwcPay = MwcPay("http://localhost:9010")
 
 # Create payment
-payment = mwcPay.createPayment("123.456", 5, 600, "http://example.com/completed", "http://example.com/received", "http://example.com/confirmed", "http://example.com/expired")
+payment = mwcPay.createPayment("123.456", 5, 600, "http://example.com/completed", "http://example.com/received", "http://example.com/confirmed", "http://example.com/expired", "notes")
 
 # Get payment info
 paymentInfo = mwcPay.getPaymentInfo(payment["payment_id"])
@@ -48,7 +48,7 @@ publicServerInfo = mwcPay.getPublicServerInfo()
    This method returns the following value:
    * `MwcPay`: An `MwcPay` object.
 
-2. MWC Pay create payment method: `createPayment(price: str | None, requiredConfirmations: int | None, timeout: int | None, completedCallback: str, receivedCallback: str | None = None, confirmedCallback: str | None = None,  expiredCallback: str | None = None) -> dict[str, str] | bool | None`
+2. MWC Pay create payment method: `createPayment(price: str | None, requiredConfirmations: int | None, timeout: int | None, completedCallback: str, receivedCallback: str | None = None, confirmedCallback: str | None = None, expiredCallback: str | None = None, notes: str | None = None, apiKey: str | None = None) -> dict[str, str] | bool | None`
 
    This method is used to create a payment and it accepts the following parameters:
    * `price: str | None`: The expected amount for the payment. If `None` then any amount will fulfill the payment.
@@ -58,32 +58,41 @@ publicServerInfo = mwcPay.getPublicServerInfo()
    * `receivedCallback: str | None` (optional): The HTTP GET request that will be performed when the payment is received. If the response status code to this request isn't `HTTP 200 OK`, then an `HTTP 500 Internal Error` response will be sent to the payment's sender when they are sending the payment. This request can't follow redirects. This request may happen multiple times despite a previous attempt receiving an `HTTP 200 OK` response status code, so make sure to prepare for this and to respond to all requests with an `HTTP 200 OK` response status code if the request has already happened. All instances of `__id__`, `__price__`, `__sender_payment_proof_address__`, `__kernel_commitment__`, and `__recipient_payment_proof_signature__` are replaced with the payment's ID, price, sender payment proof address, kernel commitment, and recipient payment proof signature respectively. If not provided or `None` then no request will be performed when the payment is received.
    * `confirmedCallback: str | None` (optional): The HTTP GET request that will be performed when the payment's number of on-chain confirmations changes and the payment isn't completed. The response status code to this request doesn't matter. This request can't follow redirects. All instances of `__id__`, and `__confirmations__` are replaced with the payment's ID and number of on-chain confirmations respectively. If not provided or `None` then no request will be performed when the payment's number of on-chain confirmations changes.
    * `expiredCallback: str | None` (optional): The HTTP GET request that will be performed when the payment is expired. If the response status code to this request isn't `HTTP 200 OK`, then the same request will be made at a later time. This request can't follow redirects. This request may happen multiple times despite a previous attempt receiving an `HTTP 200 OK` response status code, so make sure to prepare for this and to respond to all requests with an `HTTP 200 OK` response status code if the request has already happened. All instances of `__id__` are replaced with the payment's ID. If not provided or `None` then no request will be performed when the payment is expired.
+   * `notes: str | None` (optional): Text to associate with the payment.
+   * `apiKey: str | None` (optional): API key that must match the private server's API key if it's using one.
 
    This method returns the following values:
    * `dict[str, str]`: The payment was created successfully. This object contains the `payment_id: str`, `url: str`, and `recipient_payment_proof_address: str` of the created payment.
    * `False`: An error occurred on the private server and/or communicating with the private server failed.
    * `None`: Parameters are invalid.
 
-3. MWC Pay get payment info method: `getPaymentInfo(paymentId: str) -> dict[str, str | int | bool | None] | bool | None`
+3. MWC Pay get payment info method: `getPaymentInfo(paymentId: str, apiKey: str | None = None) -> dict[str, str | int | bool | None] | bool | None`
 
-   This method is used to get the status of a payment and it accepts the following parameter:
+   This method is used to get the status of a payment and it accepts the following parameters:
    * `paymentId: str`: The payment's ID.
+   * `apiKey: str | None` (optional): API key that must match the private server's API key if it's using one.
 
    This method returns the following values:
    * `dict[str, str | int | bool | None]`: This object contains the payment's `url: str`, `price: str | None`, `required_confirmations: int`, `received: bool`, `confirmations: int`, `time_remaining: int | None`, `status: str`, and `recipient_payment_proof_address: str`. The `status: str` can be one of the following values: `Expired`, `Not received`, `Received`, `Confirmed`, or `Completed`.
    * `False`: An error occurred on the private server and/or communicating with the private server failed.
    * `None`: Parameters are invalid and/or the payment doesn't exist.
 
-4. MWC Pay get price method: `getPrice() -> str | bool | None`
+4. MWC Pay get price method: `getPrice(apiKey: str | None = None) -> str | bool | None`
 
-   This method is used to get the price of MimbleWimble coin and it returns the following values:
+   This method is used to get the price of MimbleWimble coin and it accepts the following parameters:
+   * `apiKey: str | None` (optional): API key that must match the private server's API key if it's using one.
+
+   This method returns the following values:
    * `string`: The price of MimbleWimble Coin in USDT.
    * `False`: An error occurred on the private server and/or communicating with the private server failed.
    * `None`: Parameters are invalid and/or the price API is disabled on the private server.
 
-5. MWC Pay get public server info method: `getPublicServerInfo() -> dict[str, str | None] | bool | None`
+5. MWC Pay get public server info method: `getPublicServerInfo(apiKey: str | None = None) -> dict[str, str | None] | bool | None`
 
-   This method is used to get info about MWC Pay's public server and it returns the following values:
+   This method is used to get info about MWC Pay's public server and it accepts the following parameters:
+   * `apiKey: str | None` (optional): API key that must match the private server's API key if it's using one.
+
+   This method returns the following values:
    * `dict[str, str | None]`: This object contains the public server's `url: str` and `onion_service_address: str | None`.
    * `False`: An error occurred on the private server and/or communicating with the private server failed.
    * `None`: Parameters are invalid.
--- src/nicolasflamel/mwc_pay.py
+++ src/nicolasflamel/mwc_pay.py
@@ -23,10 +23,10 @@ class MwcPay:
 		self._privateServer = privateServer
 	
 	# Create payment
-	def createPayment(self, price: str | None, requiredConfirmations: int | None, timeout: int | None, completedCallback : str, receivedCallback: str | None = None, confirmedCallback: str | None = None, expiredCallback: str | None = None) -> dict[str, str] | bool | None:
+	def createPayment(self, price: str | None, requiredConfirmations: int | None, timeout: int | None, completedCallback : str, receivedCallback: str | None = None, confirmedCallback: str | None = None, expiredCallback: str | None = None, notes: str | None = None, apiKey: str | None = None) -> dict[str, str] | bool | None:
 	
 		# Check if parameters are invalid
-		if (isinstance(price, str) is False and price is not None) or (isinstance(requiredConfirmations, int) is False and requiredConfirmations is not None) or (isinstance(timeout, int) is False and timeout is not None) or isinstance(completedCallback, str) is False or (isinstance(receivedCallback, str) is False and receivedCallback is not None) or (isinstance(confirmedCallback, str) is False and confirmedCallback is not None) or (isinstance(expiredCallback, str) is False and expiredCallback is not None):
+		if (isinstance(price, str) is False and price is not None) or (isinstance(requiredConfirmations, int) is False and requiredConfirmations is not None) or (isinstance(timeout, int) is False and timeout is not None) or isinstance(completedCallback, str) is False or (isinstance(receivedCallback, str) is False and receivedCallback is not None) or (isinstance(confirmedCallback, str) is False and confirmedCallback is not None) or (isinstance(expiredCallback, str) is False and expiredCallback is not None) or (isinstance(notes, str) is False and notes is not None) or (isinstance(apiKey, str) is False and apiKey is not None):
 		
 			# Return None
 			return None
@@ -56,7 +56,13 @@ class MwcPay:
 				"confirmed_callback": confirmedCallback,
 				
 				# Expired callback
-				"expired_callback": expiredCallback
+				"expired_callback": expiredCallback,
+				
+				# Notes
+				"notes": notes,
+				
+				# API key
+				"api_key": apiKey
 				
 			}.items() if value is not None}))) as createPaymentResponse:
 			
@@ -104,10 +110,10 @@ class MwcPay:
 			return False
 	
 	# Get payment info
-	def getPaymentInfo(self, paymentId: str) -> dict[str, str | int | bool | None] | bool | None:
+	def getPaymentInfo(self, paymentId: str, apiKey: str | None = None) -> dict[str, str | int | bool | None] | bool | None:
 	
 		# Check if parameters are invalid
-		if isinstance(paymentId, str) is False:
+		if isinstance(paymentId, str) is False or (isinstance(apiKey, str) is False and apiKey is not None):
 		
 			# Return None
 			return None
@@ -116,18 +122,21 @@ class MwcPay:
 		try:
 		
 			# Send get payment info request to the private server
-			with urllib.request.urlopen("{}/get_payment_info?{}".format(self._privateServer, urllib.parse.urlencode({
+			with urllib.request.urlopen("{}/get_payment_info?{}".format(self._privateServer, urllib.parse.urlencode({key: value for key, value in {
 			
 				# Payment Id
-				"payment_id": paymentId
+				"payment_id": paymentId,
 				
-			}))) as getPaymentInfoResponse:
+				# API key
+				"api_key": apiKey
+				
+			}.items() if value is not None}))) as getPaymentInfoResponse:
 			
 				# Get payment info from get payment info response
 				paymentInfo = json.loads(getPaymentInfoResponse.read())
 				
 				# Check if payment info's URL, price, required confirmations, received, confirmations, time remaining, status, or recipient payment proof address are invalid
-				if isinstance(paymentInfo, dict) is False or "url" not in paymentInfo or isinstance(paymentInfo["url"], str) is False or paymentInfo["url"] == "" or "price" not in paymentInfo or (paymentInfo["price"] is not None and isinstance(paymentInfo["price"], str) is False) or (paymentInfo["price"] is not None and re.match("^(?:0(?:\.\d+)?|[1-9]\d*(?:\.\d+)?)$", paymentInfo["price"]) is None) or "required_confirmations" not in paymentInfo or isinstance(paymentInfo["required_confirmations"], int) is False or paymentInfo["required_confirmations"] <= 0 or "received" not in paymentInfo or isinstance(paymentInfo["received"], bool) is False or "confirmations" not in paymentInfo or isinstance(paymentInfo["confirmations"], int) is False or paymentInfo["confirmations"] < 0 or paymentInfo["confirmations"] > paymentInfo["required_confirmations"] or "time_remaining" not in paymentInfo or (paymentInfo["time_remaining"] is not None and isinstance(paymentInfo["time_remaining"], int) is False) or (paymentInfo["time_remaining"] is not None and paymentInfo["time_remaining"] < 0) or "status" not in paymentInfo or isinstance(paymentInfo["status"], str) is False or "recipient_payment_proof_address" not in paymentInfo or isinstance(paymentInfo["recipient_payment_proof_address"], str) is False or paymentInfo["recipient_payment_proof_address"] == "":
+				if isinstance(paymentInfo, dict) is False or "url" not in paymentInfo or isinstance(paymentInfo["url"], str) is False or paymentInfo["url"] == "" or "price" not in paymentInfo or (paymentInfo["price"] is not None and isinstance(paymentInfo["price"], str) is False) or (paymentInfo["price"] is not None and re.match("^(?:0(?:\\.\\d+)?|[1-9]\\d*(?:\\.\\d+)?)$", paymentInfo["price"]) is None) or "required_confirmations" not in paymentInfo or isinstance(paymentInfo["required_confirmations"], int) is False or paymentInfo["required_confirmations"] <= 0 or "received" not in paymentInfo or isinstance(paymentInfo["received"], bool) is False or "confirmations" not in paymentInfo or isinstance(paymentInfo["confirmations"], int) is False or paymentInfo["confirmations"] < 0 or paymentInfo["confirmations"] > paymentInfo["required_confirmations"] or "time_remaining" not in paymentInfo or (paymentInfo["time_remaining"] is not None and isinstance(paymentInfo["time_remaining"], int) is False) or (paymentInfo["time_remaining"] is not None and paymentInfo["time_remaining"] < 0) or "status" not in paymentInfo or isinstance(paymentInfo["status"], str) is False or "recipient_payment_proof_address" not in paymentInfo or isinstance(paymentInfo["recipient_payment_proof_address"], str) is False or paymentInfo["recipient_payment_proof_address"] == "":
 				
 					# Return false
 					return False
@@ -182,19 +191,30 @@ class MwcPay:
 			return False
 	
 	# Get price
-	def getPrice(self) -> str | bool | None:
+	def getPrice(self, apiKey: str | None = None) -> str | bool | None:
 	
+		# Check if parameters are invalid
+		if isinstance(apiKey, str) is False and apiKey is not None:
+		
+			# Return None
+			return None
+			
 		# Try
 		try:
 		
 			# Send get price request to the private server
-			with urllib.request.urlopen("{}/get_price".format(self._privateServer)) as getPriceResponse:
+			with urllib.request.urlopen("{}/get_price?{}".format(self._privateServer, urllib.parse.urlencode({key: value for key, value in {
+			
+				# API key
+				"api_key": apiKey
+				
+			}.items() if value is not None}))) as getPriceResponse:
 			
 				# Get price from get price response
 				price = json.loads(getPriceResponse.read())
 				
 				# Check if price is invalid
-				if isinstance(price, dict) is False or "price" not in price or isinstance(price["price"], str) is False or re.match("^(?:0(?:\.\d+)?|[1-9]\d*(?:\.\d+)?)$", price["price"]) is None:
+				if isinstance(price, dict) is False or "price" not in price or isinstance(price["price"], str) is False or re.match("^(?:0(?:\\.\\d+)?|[1-9]\\d*(?:\\.\\d+)?)$", price["price"]) is None:
 				
 					# Return false
 					return False
@@ -224,13 +244,24 @@ class MwcPay:
 			return False
 	
 	# Get public server info
-	def getPublicServerInfo(self) -> dict[str, str | None] | bool | None:
+	def getPublicServerInfo(self, apiKey: str | None = None) -> dict[str, str | None] | bool | None:
 	
+		# Check if parameters are invalid
+		if isinstance(apiKey, str) is False and apiKey is not None:
+		
+			# Return None
+			return None
+			
 		# Try
 		try:
 		
 			# Send get public server info request to the private server
-			with urllib.request.urlopen("{}/get_public_server_info".format(self._privateServer)) as getPublicServerInfoResponse:
+			with urllib.request.urlopen("{}/get_public_server_info?{}".format(self._privateServer, urllib.parse.urlencode({key: value for key, value in {
+			
+				# API key
+				"api_key": apiKey
+				
+			}.items() if value is not None}))) as getPublicServerInfoResponse:
 			
 				# Get public server info from get public server info response
 				publicServerInfo = json.loads(getPublicServerInfoResponse.read())
